a1b250dd85c696dfa77e94572b98ec7638465ebc
RikoRaheste <93789014+RikoRaheste@users.noreply.github.com>
2026-04-15 15:30:05 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-    attribution: 'OpenStreetMap contributors'
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: 'OpenStreetMap contributors',
 })
+
+osm.addTo(map)
 
 // add geoJSON polygons layer*
 async function addDistrictsGeoJson(url) {
@@ -24,6 +26,41 @@ async function addDistrictsGeoJson(url) {
   const data = await response.json()
   const polygons = L.geoJson(data, {
     onEachFeature: popUPinfo,
+  })
+  polygons.addTo(map)
+}
+// get color from feature property
+function getColor(property) {
+  switch (property) {
+    case 1:
+      return '#ff0000'
+    case 13:
+      return '#009933'
+    case 6:
+      return '#0000ff'
+    case 7:
+      return '#ff0066'
+    default:
+      return '#ffffff'
+  }
+}
+
+// polygon style
+function polygonStyle(feature) {
+  return {
+    fillColor: getColor(feature.properties.OBJECTID),
+    fillOpacity: 0.5,
+    weight: 1,
+    opacity: 1,
+    color: 'grey',
+  }
+}
+async function addDistrictsGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const polygons = L.geoJson(data, {
+    onEachFeature: popUPinfo,
+    style: polygonStyle,
   })
   polygons.addTo(map)
 }
