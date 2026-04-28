fcb54c913a34e2870fc4a22fe88dc420f28b23d2
RikoRaheste <93789014+RikoRaheste@users.noreply.github.com>
2026-04-28 22:28:34 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,32 +1,20 @@
-// --- Basic Map Setup ---
+// Basic Map Setup
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 
-// --- Raster Tile Layer ---
+// Raster Tile Layer
 const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: 'OpenStreetMap contributors'
 })
 osm.addTo(map)
 
-// --- Vector Layer: Tartu City Cell Towers ---
+// add celltowers vector layer
 async function addCelltowersGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
-
-  const towers = L.geoJson(data, {
-    pointToLayer: (feature, latlng) =>
-      L.circleMarker(latlng, {
-        radius: 5,
-        fillColor: 'red',
-        fillOpacity: 0.6,
-        color: 'red',
-        weight: 1
-      })
-  })
-
-  towers.addTo(map)
+  const markers = L.geoJson(data)
+  markers.addTo(map)
 }
-
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 // --- defaultMapSettings Function ---
