5dd5229d18f36bf413dc1084fc3fdc082694b148
RikoRaheste <93789014+RikoRaheste@users.noreply.github.com>
2026-04-28 22:48:20 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -23,11 +23,17 @@ function defaultMapSettings() {
 }
 
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-
 // add geoJSON layer
 async function addGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
-  console.log(data.features[0].geometry.coordinates)
-  console.log(data.features[0].properties.area)
+  const heatData = data.features.map(heatDataConvert)
+  console.log(heatData)
+}
+function heatDataConvert(feature) {
+  return [
+    feature.geometry.coordinates[1],
+    feature.geometry.coordinates[0],
+    feature.properties.area,
+  ]
 }
